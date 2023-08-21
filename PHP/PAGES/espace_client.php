04c495f2a10ac4ecc305d6fa03e912332892ea09
NHBakli <bakli25@protonmail.com>
2023-08-21 13:29:09 +0200

    modification de la page contactr et commencement de la page espace client

---
+++ PHP/PAGES/espace_client.php
@@ -0,0 +1,26 @@
+<?php session_start() ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/espace_client.css">
+    <?php include '../COMPONENTS/important_link.php' ?>
+    <title>Espace Client</title>
+</head>
+<body>
+<?php include '../COMPONENTS/header.php' ?>
+
+<main>
+    <div class="container_img">
+        
+    </div>
+    <div class="container">
+
+    </div>
+</main>
+
+<?php include '../COMPONENTS/footer.php' ?>
+</body>
+</html>
